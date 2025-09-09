01abf06ce5de131c53e4bd8069bc1194f316d68e
Eden Reich <eden.reich@gmail.com>
2025-09-09 02:04:45 +0200

    feat: Add 404 error page and session redirect handling
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (sessionStorage.redirect) {
+  history.replaceState(null, '', sessionStorage.redirect)
+  delete sessionStorage.redirect
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
