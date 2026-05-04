717f07c466bc7993e636f05aa5d895f6b5b4d4de
Eden Reich <eden.reich@gmail.com>
2026-05-04 23:53:54 +0200

    chore(deps): Bump the project to use the latest versions

--- eslint.config.js
+++ eslint.config.js
@@ -3,32 +3,20 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-  {
-    ignores: ['dist'],
-  },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-)
+])
